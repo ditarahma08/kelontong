f9a59ff7b2d2103e19df87f10913588a9fd8e210
ditarahma08 <ditarahma08@gmail.com>
2023-12-25 20:11:52 +0700

    add login component

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,25 +1,48 @@
-'use client'
-import { useState } from "react"
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Login() {
-    const [username, setUsername] = useState('')
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    return (
-        <div className="border rounded-md border-neutral-300 flex flex-col gap-y-4 w-96 p-10">
-            {/* <div className="flex justify-between">
-                <label>Username</label>
-                <input type="text" className="w-52" value={username}/>
-            </div>
-            <div className="flex justify-between">
-                <label>Password</label>
-                <input type="password" className="w-52"/>
-            </div>
+  function clickOnSubmit() {
+    router.push("/catalog");
+  }
 
-            <button>Login</button> */}
-            <form>
-                <label>Username</label>
-                <input className="input" type="text"/>
-            </form>
+  return (
+    <div className="border rounded-md border-neutral-300 w-96 p-10">
+      <div
+        className="flex flex-col gap-y-4 text-white"
+      >
+        <div>
+          <label>Username</label>
+          <input
+            className="input"
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
         </div>
-    )
+        <div>
+          <label>Password</label>
+          <input
+            className="input"
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="button is-info"
+          onClick={clickOnSubmit}
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
 }
